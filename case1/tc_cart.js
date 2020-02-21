d7f4a2bddf130b2e2cc2d22d16a7dbdebf3d18c5
Samuel <sharris163@west-mec.org>
2020-02-21 13:15:31 -0700

    finished project 1

--- case1/tc_cart.js
+++ case1/tc_cart.js
@@ -17,14 +17,17 @@ var orderTotal = 0;
 var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>";
 
 for(var i = 0; i < item.length; i++){
-   cartHTML += "<tr><td>" +<img src=" + item[i] + "alt=" + item[i] + "/></td>;
+   cartHTML += "<tr><td>" +"<img src='tc_" + item[i] + ".png'alt='" + item[i] + "'/></td>"
    cartHTML += "<td>" + itemDescription[i] + "</td><td>$" + itemPrice[i] + "</td><td>" + itemQty[i] + "</td>";
 
    var itemCost = itemPrice[i]*itemQty[i];
 
-   cartHTML += "<td>$" + itemcost + "</td></tr>";
+   cartHTML += "<td>$" + itemCost + "</td></tr>";
 
-   itemCost += orderTotal;
+   orderTotal += itemCost;
 
 }
 
+cartHTML += "<tr><td colspan='4'>Subtotal</td><td>$" + orderTotal + "</td></tr></table>"
+
+document.getElementById("cart").innerHTML = cartHTML
